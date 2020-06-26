630f57a546dbd9c92c86c379065e91c2e1a00f2f
Jian Weng <jian.weng@ucla.edu>
2020-06-26 04:43:29 +0000

    fix gem5

--- setup.sh
+++ setup.sh
@@ -5,5 +5,5 @@ export SS_STACK=$SS
 export SS_TOOLS=$SS_STACK/ss-tools
 export RISCV=$SS_TOOLS
 export PATH=$SS_TOOLS/bin:$PATH
-export PATH=$SS_STACK/gem5/build/RISCV:$PATH
+export PATH=$SS_STACK/dsa-gem5/build/RISCV:$PATH
 export LD_LIBRARY_PATH=$SS_TOOLS/lib:$LD_LIBRARY_PATH
