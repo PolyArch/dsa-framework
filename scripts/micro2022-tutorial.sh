b864050577bc1e24a8ffe24e4487d11f568ce9cf
Jian Weng <jian.weng@ucla.edu>
2022-09-22 17:37:11 -0700

    update demo script

--- scripts/micro2022-tutorial.sh
+++ scripts/micro2022-tutorial.sh
@@ -1,9 +1,28 @@
 ###########################################
 # Software Stack and Programming Interface
 ###########################################
-cd $SS/dsa-apps/demo
+cd $SS/dsa-apps/sdk/compiled
 
 ## Run gem5 simulation
+make vecadd.ll
+make ss-vecadd.ll
+make ss-vecadd.s
+make ss-vecadd.out
+./run.sh ss-vecadd.out
+
+## DFG Visualization
+ss_sched vecadd_0_1.dfg -f
+neato -Tpng -overlap=false -Gepsilon=.0001 -o vecadd_0_1_1.png vecadd_0_1.gv
+
+## ADG Visualization
+python3 $SS/scripts/adg_visualization.py generators/dsagen2/adg/Mesh7x5-Simple64-Full7I5O.json
+
+## Scheduled DFG-ADG Visualization
+ss_sched vecadd_0_1.dfg ../adg/Mesh7x5-Simple64-Full7I5O.json -f
+dot -Tpng -o vecadd_0_1.png vecadd_0_1.gv
+
+## Demo a MV-Suite
+cd $SS/dsa-apps/demo
 ./run.sh ss-mv.out
 ./run.sh ss-crs.out
 
@@ -12,14 +31,6 @@ make ultraclean
 make ss-mv.riscv
 make ss-crs.riscv
 
-## DFG Visualization
-ss_sched spmv_0_1_1.dfg -f
-neato -Tpng -overlap=false -Gepsilon=.0001 -o spmv_0_1_1.png spmv_0_1_1.gv
-
-## Scheduled DFG-ADG Visualization
-ss_sched spmv_0_1_1.dfg ../adg/Mesh7x5-Simple64-Full7I5O.json -f
-dot -Tpng -o spmv_0_1_1.png spmv_0_1_1.gv
-
 ###########################################
 # Hardware Stack and RTL Simulation
 ###########################################
@@ -27,9 +38,6 @@ cd $SS/chipyard
 
 ## Compile Verilator RTL Simulation File
 make -C sims/verilator CONFIG=MeshDSARocketConfig
-
-## ADG Visualization
-python3 $SS/scripts/adg_visualization.py generators/dsagen2/adg/Mesh7x5-Simple64-Full7I5O.json
 
 ## Compile all unit tests
 cd $SS/chipyard/generators/dsagen2
